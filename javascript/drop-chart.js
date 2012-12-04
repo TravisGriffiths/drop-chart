2d65c938b7af8143c811f17f7ab3cc612fe386ee
Rachel <rachel@quant5.com>
2012-12-03 22:14:50 -0500

    playing with palattes

--- javascript/drop-chart.js
+++ javascript/drop-chart.js
@@ -165,7 +165,7 @@
         function paletteFactory() {
           this.palettes['general'] = function(i) {
             var colors;
-            colors = ['#ea7db0', '#df9ac4', '#ea4f4f', '#c56156', '#53548e', '#747dbc', '#bec2e2', '#3e499f', '#747dbc', '#bec2e2', '#3e499f', '#53548e', '#8ecea5', '#106735', '#2bb673', '#d7ecdd', '#f6dcc6', '#e8b087', '#d16a28', '#d88349', '#766692', '#845194', '#6f2a82', '#b59bc2'];
+            colors = ["#ea4f4f", "#3e499f", "#cece29", "#6FCC3A", "#6f2a82", "#106735", "#d16a28", "#c56156", "#53548e", "#d8d65a", "#845194", "#2bb673", "#d88349", "#ea7db0", "#747dbc", "#e0de84", "#766692", "#8ecea5", "#e8b087", "#df9ac4", "#bec2e2", "#f7f385", "#b59bc2", "#d7ecdd", "#f6dcc6"];
             return colors[i % colors.length];
           };
           this.palettes['category10'] = d3.scale.category10();
